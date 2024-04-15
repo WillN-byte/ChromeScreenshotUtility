d2ae509a54f80386b46b6050b347cb77f06843f7
uni-dos <uni.dos12@outlook.com>
2024-04-15 16:19:14 -0700

    moved date timestamp to own function

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function testClick(data, tab){
       singlePage();
       break;
     case 'region':
-      selectRegion();
+      selectRegion(tab);
       break;
     default:
       console.log(data.menuItemId);
@@ -56,44 +56,28 @@ function testClick(data, tab){
     console.log("pressed");
 }
 
-// don't hard code name; need a settings page
-function singlePage() {
+function getFileStamp() {
   let currentDate = new Date();
-
   let date = (currentDate.getMonth() + 1) + 
     "-" + (currentDate.getDate()) + "-" + (currentDate.getFullYear()) 
       + " " + (currentDate.getHours() + "" + (currentDate.getMinutes())
       + "" + (currentDate.getSeconds()));
   console.log(date);
+  return `screenshot ${date}.png`;
+}
+
+// don't hard code name; need a settings page
+function singlePage() {
+  
   chrome.tabs.captureVisibleTab(null, { format: "png" }, function(screenshotUrl) {
-    chrome.downloads.download({filename: `screenshot ${date}.png`, url: screenshotUrl}, function(id){
+    chrome.downloads.download({filename: getFileStamp(), url: screenshotUrl}, function(id){
       console.log("error" + id);
     });
 });
 }
 
 // here send a message so we can tell the content script to draw the canvas 
-function selectRegion() {
+function selectRegion(tab) {
+  // chrome.tabs.sendMessage(tab.id, {message: "region screenshot"}, (response) =>{console.log("Done"); return true;})
     
-
-  // chrome.desktopCapture.chooseDesktopMedia(["tab"], tab, function(streamId) {
-  //   if (streamId) {
-  //     console.log("Success");
-  //     // var video = document.createElement("video");
-  //     // video.src = URL.createObjectURL(streamId);
-  //     // video.play();
-  
-  //     // video.onloadedmetadata = function() {
-  //     //   var canvas = document.createElement("canvas");
-  //     //   canvas.width = video.videoWidth;
-  //     //   canvas.height = video.videoHeight;
-  //     //   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
-  
-  //     //   var screenshotUrl = canvas.toDataURL("image/png");
-  //     //   var image = new Image();
-  //     //   image.src = screenshotUrl;
-  //     //   document.body.appendChild(image);
-  //     // };
-  //   }
-  // });
 }
